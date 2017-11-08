af0aa86db777d1fbc03804d7db5e3a9744aefae4
saulve <saul.venskutonis@adenin.com>
2017-11-08 11:09:58 +0000

    Test dynamic card embed

---
+++ test.js
@@ -0,0 +1,4 @@
+let card = document.createElement('include-card');
+card.setAttribute('name', 'atm-pto-status');
+let container = document.getElmenentById('card-container');
+container.appendChild(card);
